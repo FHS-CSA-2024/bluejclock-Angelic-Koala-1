afd474e118f85f37a48f2b44728f3a95c5dc2bce
Angelic-Koala <118229343+Angelic-Koala@users.noreply.github.com>
2024-09-10 09:37:28 -0700

    Update ClockDisplay12Hour.java

--- src/main/java/ClockDisplay12Hour.java
+++ src/main/java/ClockDisplay12Hour.java
@@ -1,6 +1,1 @@
-
-public class ClockDisplay12Hour
-{
-    //Extend ClockDisplay for a 12 hour clock. This means you need to track AM/PM
-    //Update all methods and parameters to accomodate this change
-}
+I combined the seconds and 12 hours clock into 1 clock and asked you about it, I think you said it was ok since I still wrote the same code/functionality, it was just combined.
